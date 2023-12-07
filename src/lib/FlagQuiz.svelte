37cf2a76b4dd774d92969080c7d3b278776e999a
Barrett Osmundson <barrettosmundson@gmail.com>
2023-12-07 11:56:36 -0500

    mobile and flag quiz changes 2

--- src/lib/FlagQuiz.svelte
+++ src/lib/FlagQuiz.svelte
@@ -28,42 +28,39 @@
   randomFlag();
 </script>
 
-<div class="container flex-1 p-4 h-full">
   
   <!-- {#each flags as flag, i }
-    <h4 class="h2 mb-8 flex-0"> {i}: {flag.name} </h4>
+    <h4 class="h2 mb-8 flex-none"> {i}: {flag.name} </h4>
     <img src={flag.url} width=200 />
     <hr class="m-2" />
   {/each} -->
 
 
-  <div class="card px-4 py-32 lg:py-4 flex flex-col justify-around lg:justify-between items-center h-full">
-    <h4 class="h2 mb-8 flex-0 align-middle {revealed ? '' : 'placeholder py-5 animate-pulse w-8/12 lg:w-1/2'}"> {revealed ? flag.name : ''} </h4>
-
-    <div class="flex-1 w-full bg-no-repeat bg-center bg-contain mb-8 lg:p-24" style="background-image: url('{flag.url}');"></div>
-
-    <div class="w-full flex-0 flex space-x-8 justify-center px-4 lg:px-8">
-      <button type="button" class="btn variant-filled py-4 w-3/4" on:click={revealed ? randomFlag : reveal}>
-        <span>
-          {#if revealed}
-            <IconReload width="32" height="32" />
-          {:else}
-            <IconEye width="32" height="32"/>
-          {/if}
-        </span>
-        <span class="h3 align-middle">{revealed ? 'Reload' : 'Reveal'}</span>
-      </button>
-
-      <!-- <button type="button" class="btn variant-filled py-4 w-full" on:click={randomFlag}>
-        <span><IconReload width="32" height="32" /></span>
-        <span class="h3">Reload</span>
-      </button> -->
-    </div>
-    
+<div class="card px-4 py-4 flex flex-col justify-around lg:justify-between items-center h-full">
+  <div class="h-1/6 lg:h-auto mb-8 flex-0 align-middle {revealed ? '' : 'placeholder animate-pulse w-8/12 lg:w-1/2 lg:py-5'}">
+    <h2 class="h2"> {revealed ? flag.name : ''} </h2>
   </div>
 
+  <div class="flex-1 w-full bg-no-repeat bg-center bg-contain mb-8 lg:p-24" style="background-image: url('{flag.url}');"></div>
 
+  <div class="flex-none lg:px-8 h-1/6 ">
+    <button type="button" class="btn variant-filled py-4" on:click={revealed ? randomFlag : reveal}>
+      <span>
+        {#if revealed}
+          <IconReload width="32" height="32" />
+        {:else}
+          <IconEye width="32" height="32"/>
+        {/if}
+      </span>
+      <span class="h3 align-middle text-clip">{revealed ? 'Reload' : 'Reveal'}</span>
+    </button>
 
+    <!-- <button type="button" class="btn variant-filled py-4 w-full" on:click={randomFlag}>
+      <span><IconReload width="32" height="32" /></span>
+      <span class="h3">Reload</span>
+    </button> -->
+  </div>
+  
 </div>
 
 
--- src/routes/apps/+page.svelte
+++ src/routes/apps/+page.svelte
@@ -11,8 +11,8 @@
 
 </script>
 
-<div class="card bg-surface-50-900-token rounded-none flex h-full">
-    <AppRail class="flex-0 h-full">
+<div class="card bg-surface-50-900-token rounded-none flex h-full w-full">
+    <AppRail class="flex-none h-full">
         <!-- Lead -->
         <svelte:fragment slot="lead">
             <AppRailAnchor>
@@ -24,14 +24,14 @@
             <svelte:fragment slot="lead"><div class="flex justify-center"> <IconFlagQuestion width="32" height="32" /> </div></svelte:fragment>
             <span>Flag Quiz</span>
         </AppRailTile>
-        <AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
+        <!-- <AppRailTile bind:group={currentTile} name="tile-2" value={1} title="tile-2">
             <svelte:fragment slot="lead">(icon)</svelte:fragment>
             <span>Tile 2</span>
         </AppRailTile>
         <AppRailTile bind:group={currentTile} name="tile-3" value={2} title="tile-3">
             <svelte:fragment slot="lead">(icon)</svelte:fragment>
             <span>Tile 3</span>
-        </AppRailTile>
+        </AppRailTile> -->
         <!-- Trail -->
         <svelte:fragment slot="trail">
             <AppRailAnchor>
@@ -39,14 +39,17 @@
             </AppRailAnchor>
         </svelte:fragment>
     </AppRail>
-    <div class="w-full flex-1 flex flex-col">
-        <AppBar class="w-full flex-0">
+    <div class="flex-1 flex flex-col overflow-hidden">
+        <AppBar class="w-full flex-none">
             <svelte:fragment slot="lead"><IconArrowLeft width="24" height="24" /></svelte:fragment>
             <h4 class="h4">Flag Quiz</h4>
             <svelte:fragment slot="trail">(actions)</svelte:fragment>
         </AppBar>
-        {#if currentTile == 0}
-            <FlagQuiz></FlagQuiz>
-        {/if}
+        <div class="w-full h-full p-4 flex-1">
+            {#if currentTile == 0}
+                <FlagQuiz></FlagQuiz>
+            {/if}
+        </div>
+       
     </div>
 </div>
