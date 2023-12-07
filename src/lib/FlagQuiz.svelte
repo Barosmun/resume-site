25222521e070ba92a70623ce90a2fa0fedde20aa
Barrett Osmundson <barrettosmundson@gmail.com>
2023-12-06 22:05:02 -0500

    flag quiz update

--- src/lib/FlagQuiz.svelte
+++ src/lib/FlagQuiz.svelte
@@ -3,12 +3,26 @@
 
   // Icons
   import IconReload from '~icons/tabler/reload';
+  import IconEye from '~icons/tabler/eye';
   
   let flag: {name: string, url: string};
 
+  let revealed = false;
+
   function randomFlag(){
-    let rand = Math.floor(Math.random() * flags.length);
+    // let max = flags.length;
+    // 194 = Zimbabwe
+    // 195 = Kosovo
+    // 196 = Taiwan
+    let max = 194 + 1;
+
+    let rand = Math.floor(Math.random() * max);
     flag = flags[rand];
+    revealed = false;
+  }
+
+  function reveal(){
+    revealed = true;
   }
 
   randomFlag();
@@ -16,19 +30,38 @@
 
 <div class="container flex-1 p-4 h-full">
   
-  <!-- {#each flags as flag } -->
+  <!-- {#each flags as flag, i }
+    <h4 class="h2 mb-8 flex-0"> {i}: {flag.name} </h4>
+    <img src={flag.url} width=200 />
+    <hr class="m-2" />
+  {/each} -->
+
+
   <div class="card p-4 flex flex-col justify-between items-center h-full">
-    <h4 class="h2 mb-8 flex-0"> {flag.name} </h4>
+    <h4 class="h2 mb-8 flex-0 align-middle {revealed ? '' : 'placeholder py-5 animate-pulse w-1/2'}"> {revealed ? flag.name : ''} </h4>
 
     <div class="flex-1 w-full bg-no-repeat bg-center bg-contain mb-8" style="background-image: url('{flag.url}');"></div>
 
+    <div class="w-full flex-0 flex space-x-8 justify-center px-8">
+      <button type="button" class="btn variant-filled py-4 w-3/4" on:click={revealed ? randomFlag : reveal}>
+        <span>
+          {#if revealed}
+            <IconReload width="32" height="32" />
+          {:else}
+            <IconEye width="32" height="32"/>
+          {/if}
+        </span>
+        <span class="h3 align-middle">{revealed ? 'Reload' : 'Reveal'}</span>
+      </button>
 
-    <button type="button" class="btn variant-filled py-4 w-full flex-0" on:click={randomFlag}>
-      <span><IconReload width="32" height="32" /></span>
-      <span class="h3">Reload</span>
-    </button>
+      <!-- <button type="button" class="btn variant-filled py-4 w-full" on:click={randomFlag}>
+        <span><IconReload width="32" height="32" /></span>
+        <span class="h3">Reload</span>
+      </button> -->
+    </div>
+    
   </div>
-  <!-- {/each} -->
+
 
 
 </div>
