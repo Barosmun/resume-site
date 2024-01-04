5fb5dc45de2a527da3dd7399e90a0fc91e956874
Barrett Osmundson <barrettosmundson@gmail.com>
2024-01-04 10:27:09 -0500

    mobile header adjustment

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 
 <AppShell>
   <svelte:fragment slot="header">
-    <AppBar padding="px-8 pt-0 pb-10 md:pt-6 md:px-0" gridColumns="grid-cols-3 md:grid-cols-11" slotLead="md:col-start-4 md:place-self-center" slotDefault="md:col-start-6 place-self-center" slotTrail="place-content-end md:col-start-8 md:col-span-2 md:place-content-start">
+    <AppBar padding="px-8 pt-0 pb-12 md:pt-6 md:px-0" gridColumns="grid-cols-3 md:grid-cols-11" slotLead="md:col-start-4 md:place-self-center" slotDefault="md:col-start-6 place-self-center" slotTrail="place-content-end md:col-start-8 md:col-span-2 md:place-content-start">
 
       <svelte:fragment slot="lead">
         <Avatar src="{Logo}" width="w-16" rounded="rounded-full" background="none" />
